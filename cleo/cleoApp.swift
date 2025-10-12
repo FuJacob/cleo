dd16ad3298182ec2d4b6c3bbb89b6342dd303c1c
Jacob Fu <141651335+FuJacob@users.noreply.github.com>
2025-10-11 21:25:26 -0400

    Initial Commit

---
+++ cleo/cleoApp.swift
@@ -0,0 +1,17 @@
+//
+//  cleoApp.swift
+//  cleo
+//
+//  Created by Jacob Fu on 2025-10-11.
+//
+
+import SwiftUI
+
+@main
+struct cleoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
